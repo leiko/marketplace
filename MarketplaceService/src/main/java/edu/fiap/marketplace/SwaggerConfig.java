96491ff9eee95f15bfac7ec7d4a988230e18ce29
Leiko <leticia.lkonno@gmail.com>
2019-10-21 10:59:40 -0300

    add frontend

--- MarketplaceService/src/main/java/edu/fiap/marketplace/SwaggerConfig.java
+++ MarketplaceService/src/main/java/edu/fiap/marketplace/SwaggerConfig.java
@@ -2,8 +2,6 @@ package edu.fiap.marketplace;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,21 +11,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {                                    
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("edu.fiap.marketplace"))
-                .paths(PathSelectors.regex("/.*"))
-                .build();
-             
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("edu.fiap.marketplace"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
-  @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-}	
+}
